0f35c0ede3e23052d2df27b43776df288d107775
shahinexy <shahinexy@gmail.com>
2024-02-13 15:22:28 +0600

    work on multi function

--- js/dom.js
+++ js/dom.js
@@ -1,7 +1,7 @@
 document.getElementById('btn').addEventListener('click', function(){
-    const firstInput = document.getElementById('fisrt_input').value;
+    const firstInput = document.getElementById('base').value;
     const firstValue = parseFloat(firstInput);
-    const secondtInput = document.getElementById('secont_input').value;
+    const secondtInput = document.getElementById('height').value;
     const secondtValue = parseFloat(secondtInput);
     const result =  0.5 * firstValue * secondtValue;
     
@@ -12,3 +12,21 @@ document.getElementById('btn').addEventListener('click', function(){
     document.getElementById('result').innerText = result;
     }
 })
+
+// Another way
+// If you have to done this same think multiple time this is a good way to done this.
+function getFirstvallue(fisrtinput){
+    const firstInput = document.getElementById(fisrtinput).value;
+    const firstValue = parseFloat(firstInput);
+    return firstValue;
+}
+
+function getsecondvallue(secondinput){
+    const secondInput = document.getElementById(secondinput).value;
+    const secondtValue = parseFloat(secondInput);
+    return secondtValue;
+}
+function getResult(resultInput){
+    const result = document.getElementById(resultInput).innerText;    
+    return result;
+}
